c1b86cdedde097855cc96fb7646ed2e443d60b60
osvaldo <aquino35173@gmail.com>
2021-07-21 17:32:27 -0400

    comment state class for testing

--- src/Communication/main.cpp
+++ src/Communication/main.cpp
@@ -1,13 +1,13 @@
 #include "ElevatorSystem/Elevator.h"
-#include "States/InitialState.h"
+//#include "States/InitialState.h"
 
 String serData;
 Elevator* elevator = new Elevator(); 
-State* initialState = new InitialState(elevator, (uint8_t) 1, (uint8_t) 12, (uint8_t) 25);
+//State* initialState = new InitialState(elevator, (uint8_t) 1, (uint8_t) 12, (uint8_t) 25);
 
 void setup() {
   Serial.begin(9600);
-  elevator->setState(initialState);
+  //elevator->setState(initialState);
   Serial.println("Welcome to the Elevator System Prototype!");
   delay(2000);
 }
@@ -19,6 +19,5 @@ void loop() {
       elevator->setDoorStatus(true);
       Serial.println(elevator->isDoorStatus());
       delay(5000);
-
   }
 }
