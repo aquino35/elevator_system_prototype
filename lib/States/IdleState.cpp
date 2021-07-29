a8f4e4309cb36bfdd9383d39b5fb348f6b74ab64
osvaldo <aquino35173@gmail.com>
2021-07-29 04:12:30 -0400

    IdleState tweaks (started energy saving method, etc)

--- lib/States/IdleState.cpp
+++ lib/States/IdleState.cpp
@@ -2,6 +2,8 @@
 
 IdleState::IdleState(Elevator* elev){
     this->elev = elev;
+    elev->setDoorStatus(true);
+    elev-> setLightStatus(true);
 }
 
 void IdleState::load(uint16_t weight){
@@ -12,10 +14,21 @@ void IdleState::load(uint16_t weight){
 void IdleState::unload(uint16_t weight){
     if(weight > elev->getMaxLoadWeight()) //goes into negative numbers
     {
-        Serial.print("Can't unload more weight than is currently present!");
+        Serial.println("Can't unload more weight than is currently present!");
         return;
     }
     uint16_t toUnload = elev->getLoadWeight() - weight;
     elev->setLoadWeight(toUnload);
+}
+
+void IdleState::energySaving(){
+
+    unsigned long seconds = 1000L;
+    unsigned long minute = seconds * 60;
+    delay(minute); // 1 minute delay
+    elev-> setDoorStatus(false);
+    if(elev->getLoadWeight()<0){
+        elev->setLightStatus(false);
+    }
 }
 
