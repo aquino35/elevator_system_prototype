f9dc137c95918e1021bc09eb60c3544d90f8e19a
osvaldo <aquino35173@gmail.com>
2021-07-29 02:43:36 -0400

    Adjusting Comm for Idle State testing

--- src/Communication/main.cpp
+++ src/Communication/main.cpp
@@ -3,7 +3,7 @@
 #include "IdleState.h"
 
 String serData;
-uint16_t serInt;
+int serInt;
 
 Elevator* elevator = new Elevator(); 
 InitialState* initialState = new InitialState(elevator, (uint8_t) 1, (uint8_t) 12, (uint8_t) 25);
@@ -12,43 +12,45 @@ IdleState* idleState = new IdleState(elevator);
 void setup() {
   Serial.begin(9600);
   elevator->setState(initialState);
-  Serial.println("Welcome to the Elevator System Prototype!");
+  Serial.println("-- Welcome to the Elevator System Prototype! --");
   delay(2000);
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    
-      serData = Serial.readString();
-      Serial.print("Command executed: ");
-      elevator->setDoorStatus(true);
-      Serial.println((elevator->isDoorStatus()) ? "Door is open." : "Door is closed.");
 
-      //Testing Initial State
-      Serial.print("Command executed: ");
-      Serial.println("Current elevator State: " + elevator->getState());
-      delay(5000);
+    serData = Serial.readStringUntil('\n');
+    Serial.print("Command executed: ");
+    elevator->setDoorStatus(true);
+    Serial.println((elevator->isDoorStatus()) ? "Door is open." : "Door is closed.");
 
-      //Testing Idle State
-      elevator->setState(idleState);
-      Serial.print("Command executed: ");
-      Serial.println("Current elevator State: " + elevator->getState());
-      delay(2000);
+    //Testing Initial State
+    serData = Serial.readStringUntil('\n');
+    Serial.print("Command executed: ");
+    Serial.println("Current elevator State: " + elevator->getState());
+    delay(5000);
 
-      //Testing load
-      serData = Serial.readString();
-      serInt = (uint16_t) serData.toInt();
-      Serial.print("Command executed: ");
-      idleState->load(serInt);
-      Serial.println("Current elevator Weight: " + elevator->getLoadWeight());
-      delay(2000);
+    //Testing Idle State
+    serData = Serial.readStringUntil('\n');
+    elevator->setState(idleState);
+    Serial.print("Command executed: ");
+    Serial.println("Current elevator State: " + elevator->getState());
+    delay(2000);
 
-      //Testing unloading of elevator
-      serData = Serial.readString();
-      serInt = (uint16_t) serData.toInt();
-      idleState->unload(serInt);
-      Serial.print("Command executed: ");
-      Serial.println("Current elevator Weight: " + elevator->getLoadWeight());
-      delay(5000);
+    //Testing load
+    serInt = Serial.parseInt();
+    serInt = (uint16_t) serInt;
+    idleState->load(serInt);
+    Serial.print("Current elevator Weight: ");
+    Serial.println(elevator->getLoadWeight());
+    delay(2000);
+  
+    //Testing unloading of elevator
+    serInt = Serial.parseInt();
+    serInt = (uint16_t) serInt;
+    idleState->unload(serInt);
+    Serial.print("Current elevator Weight: ");
+    Serial.println(elevator->getLoadWeight());
+    delay(5000);
     }
 }
