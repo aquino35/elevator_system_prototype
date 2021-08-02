40d5d57f42511bef19c2dbfb7c78d4bd3d13ef21
anaribon <ana.ribon@upr.edu>
2021-08-02 12:10:41 -0400

    Maintenance idea update

---
+++ lib/States/MaintenanceState.cpp
@@ -0,0 +1,5 @@
+// #include "MaintenanceState.h"
+
+MaintenanceState::MaintenanceState ()
+
+//     WorkMaintenance()
--- lib/States/MaintenanceState.h
+++ lib/States/MaintenanceState.h
@@ -1,1 +1,34 @@
-//first push:)
+#pragma once
+#include "State.h"
+#include "Elevator.h"
+
+// //Special input that indicates this state
+// //all prev commands must be cleared and no other command will be processed
+// //the only command it must react to is the one that turns the state off and moves to idle state back again. 
+
+// //if "nombredelavariabledelbotondemaintenance" true, then activate this state.
+class MaintenanceState{
+    private:
+        boolean Option; //optionOn must be equal to an input in the main file
+        String stateName = "Maintenance";
+    
+    public: 
+        bool WorkMaintenance(){
+            //if opcion de maintenance en el main se elige
+            return Option = true;
+            //if apagado
+            return Option = false;
+        }
+
+        MaintenanceState(boolean OptionOn, boolean OptionOff){ 
+           if Option = true;
+           //que no se puedan recibir comandos
+        
+           else:
+            OptionOff == true:
+        }
+
+        String currentState(){return this->stateName;}
+
+        
+}
