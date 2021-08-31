bbcb88de26f55d3d417213f77e49f830aee9934a
osvaldo <aquino35173@gmail.com>
2021-08-31 16:30:49 -0400

    Small COBS change

--- lib/Server/SystemServer.cpp
+++ lib/Server/SystemServer.cpp
@@ -27,12 +27,12 @@ void Server::transmitData(unsigned char* msgBuffer, uint8_t msgSize)
     size_t bytesToSend = msgSize;
     size_t encoded_buffer_length = (msgSize) + (msgSize) / 254 + 2; 
     uint8_t *encoded_buffer = (uint8_t *) malloc(encoded_buffer_length); 
-    size_t encodedMsg = cobs_encode( msgBuffer, msgSize, encoded_buffer );
+    size_t encodedLength = cobs_encode( msgBuffer, msgSize, encoded_buffer );
 
     
     while(bytesToSend)
     {
-        bytesToSend -= Serial.write(msgBuffer, msgSize);
+        bytesToSend -= Serial.write(encoded_buffer, encodedLength);
 
         if (bytesToSend > msgSize)
         {
