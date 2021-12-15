4e26ebb0120079d5637aab15fba8a6e2a1af0c08
Yariel <yariel.then@gmail.com>
2021-12-15 17:01:11 -0400

    2 elevs

--- src/Communication/main.cpp
+++ src/Communication/main.cpp
@@ -3,18 +3,25 @@
 
 
 SystemServer* server = new SystemServer();
-Elevator* elev = new Elevator(1);
-FSM* state_machine = new FSM(elev);
+Elevator* elev_ard_1 = new Elevator(1);
+Elevator* elev_ard_2 = new Elevator(2);
+
+FSM* state_machine_ard_1 = new FSM(elev_ard_1);
+FSM* state_machine_ard_2 = new FSM(elev_ard_2);
+
 
 void setup(void) 
 {
   server->setup();
-  state_machine->setup();
+  state_machine_ard_1->setup();
+  state_machine_ard_2->setup();
 }
 
 
 void loop() 
 {
   server->loop();
-  state_machine->energy_update();
+  state_machine_ard_1->energy_update();
+  state_machine_ard_2->energy_update();
+
 }
